2b2bb8c0248f5f58f171b2c818b29e6dede89648
Reinr <blackwhitenxt@gmail.com>
2024-09-27 21:03:02 +0700

    path fix for build and deploy

---
+++ my-app/src/Header.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+const Header = () => {
+  useEffect(() => {
+    document.title = "Let us check";
+  }, []);
+  return <></>;
+};
+export default Header;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import FunctionMenu from './FunctionMenu';
-import CheckPage from './CheckPage';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import FunctionMenu from "./FunctionMenu";
+import Header from "./Header";
+import CheckPage from "./CheckPage";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <CheckPage />
   </React.StrictMode>
 );
